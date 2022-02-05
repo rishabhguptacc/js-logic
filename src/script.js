4d3651b802e55b8189c40822db44ac64f13c8199
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-05 15:04:59 +0530

    Golf Code program is written.

--- src/script.js
+++ src/script.js
@@ -1,22 +1,28 @@
-function testSize(num) {
-    // Only change code below this line
-  if(num < 5)
-  return "Tiny";
-  else if(num < 10)
-  return "Small";
-  else  if(num < 15)
-  return "Medium";
-  else  if(num < 20)
-  return "Large";
-  else  if(num >= 20)
-  return "Huge";
-  
-  
-  
-  
-  else
-    return "Change Me";
-    // Only change code above this line
-  }
-  
-  testSize(7);
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, stroke) {
+  // Only change code below this line
+    if (stroke == 1)
+    return "Hole-in-one!"
+    else if (stroke <= (par-2) 	)
+    return "Eagle"
+    else if (stroke == (par-1))
+    return "Birdie"
+    else if (stroke == (par))
+    return "Par"
+    else if (stroke == (par+1))
+    return "Bogey"
+    else if (stroke == (par+2))
+    return "Double Bogey"
+    else if (stroke >= (par+3))
+    return "Go Home!"
+    else 
+
+
+
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
