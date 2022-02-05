58f5bbd12b8bcf5209e62d054a7bd2b69d09cf63
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-05 12:47:28 +0530

    Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
-// Only change code below this line
-const myName = "Rishabh";
-const myStr = "My name is " + myName + " and I am well!";
+// Change code below this line
+const someAdjective = "Super smooth at here!";
+let myStr = "Learning to code is ";
+
+myStr += SomeAdjective;
