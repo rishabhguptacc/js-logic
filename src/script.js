71858641e9b081b339fc92e528cd4c6d1c5eec76
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-05 13:34:32 +0530

    Added ["Paul", 35] to the beginning of the myArray variable using unshift().

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 // Setup
 const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
 
 // Only change code below this line
 
-var removedFromMyArray = myArray.shift();
+myArray.unshift(["Paul", 35]);
