67cd8dfed1f57fe55b08400a4749d96f8001b486
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-05 13:37:51 +0530

    Created Shopping List.
    The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,1 @@
-// Setup
-const myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-
-// Only change code below this line
-
-myArray.unshift(["Paul", 35]);
+const myList = [["Chocolate Bar", 15],["Chocolate cake", 5],["Chocolate milk shake", 25],["Magic Bar", 19],["Five*", 15]];
