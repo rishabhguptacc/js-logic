ffb4275df6453243410463b26d4a049c4a473dd9
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-05 15:09:59 +0530

    Write a switch statement which tests val and sets answer for the following conditions: 1 - alpha 2 - beta 3 - gamma 4 - delta

--- src/script.js
+++ src/script.js
@@ -1,28 +1,25 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-
-function golfScore(par, stroke) {
-  // Only change code below this line
-    if (stroke == 1)
-    return "Hole-in-one!"
-    else if (stroke <= (par-2) 	)
-    return "Eagle"
-    else if (stroke == (par-1))
-    return "Birdie"
-    else if (stroke == (par))
-    return "Par"
-    else if (stroke == (par+1))
-    return "Bogey"
-    else if (stroke == (par+2))
-    return "Double Bogey"
-    else if (stroke >= (par+3))
-    return "Go Home!"
-    else 
-
-
-
-
-  return "Change Me";
-  // Only change code above this line
-}
-
-golfScore(5, 4);
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch(val){
+  case 1:
+  return "alpha";
+    break;
+  case 2:
+  return "beta";
+    break;
+    case 3:
+    return "gamma";
+    break;
+  case 4:
+  return "delta";
+    break;
+  
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
