4db224e4bfe285f79f4d25d458f5586bd4f9d0ac
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-05 13:15:12 +0530

    Modified the new array myArray so that it contains both a string and a number (in that order).

--- src/script.js
+++ src/script.js
@@ -1,8 +1,2 @@
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-
 // Only change code below this line
-const wordBlanks = "I saw "+ myNoun +". A "+ myAdjective +" one "+ myVerb + " very "+myAdverb ; // Change this line
-// Only change code above this line
+const myArray = ["Hi" , 5];
